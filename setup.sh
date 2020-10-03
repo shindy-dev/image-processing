650605f4a71cb9584ea18e42b883f792e50907ec
shindy-dev <shindy.developer@gmail.com>
2020-10-03 16:46:33 +0900

    windows(bash環境のみ), linux 対応

--- setup.sh
+++ setup.sh
@@ -8,14 +8,32 @@ echo "<<<  Start Setup.  <<<"
 
 # venv が作成されていれば venv は作成しない
 if [ ! -e $VENVPATH ]; then
-    python3 -m venv $VENVPATH
-    echo "Create venv (at $ABS_VENVPATH)"
+    if [ "$(uname)" == "Darwin" ]; then
+        python3 -m venv $VENVPATH
+        echo "Create venv (at $ABS_VENVPATH)"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+        python -m venv $VENVPATH
+        echo "Create venv (at $ABS_VENVPATH)"
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        python3 -m venv $VENVPATH
+        echo "Create venv (at $ABS_VENVPATH)"
+    else
+        "Unknown OS"
+    fi
 else
     echo "[Warning]: Already exist venv! Please remove $VENVPATH if you want to setup again."
 fi
 
 # venv を有効化
-source "$VENVPATH/bin/activate"
+if [ "$(uname)" == "Darwin" ]; then
+    source "$VENVPATH/bin/activate"
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    . venv/Scripts/activate
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    source "$VENVPATH/bin/activate"
+else
+    "Unknown OS"
+fi
 
 # black: Python コードフォーマッター
 # pylint: コード解析
