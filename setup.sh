01d4139816386cb6ee10295936f2f2aa1edccc6b
shindy-dev <shindy.developer@gmail.com>
2020-10-04 15:29:26 +0900

    Revert "windows(bash環境のみ), linux 対応"
    This reverts commit 650605f4a71cb9584ea18e42b883f792e50907ec.

--- setup.sh
+++ setup.sh
@@ -8,32 +8,14 @@ echo "<<<  Start Setup.  <<<"
 
 # venv が作成されていれば venv は作成しない
 if [ ! -e $VENVPATH ]; then
-    if [ "$(uname)" == "Darwin" ]; then
-        python3 -m venv $VENVPATH
-        echo "Create venv (at $ABS_VENVPATH)"
-    elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-        python -m venv $VENVPATH
-        echo "Create venv (at $ABS_VENVPATH)"
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        python3 -m venv $VENVPATH
-        echo "Create venv (at $ABS_VENVPATH)"
-    else
-        "Unknown OS"
-    fi
+    python3 -m venv $VENVPATH
+    echo "Create venv (at $ABS_VENVPATH)"
 else
     echo "[Warning]: Already exist venv! Please remove $VENVPATH if you want to setup again."
 fi
 
 # venv を有効化
-if [ "$(uname)" == "Darwin" ]; then
-    source "$VENVPATH/bin/activate"
-elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-    . venv/Scripts/activate
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    source "$VENVPATH/bin/activate"
-else
-    "Unknown OS"
-fi
+source "$VENVPATH/bin/activate"
 
 # black: Python コードフォーマッター
 # pylint: コード解析
